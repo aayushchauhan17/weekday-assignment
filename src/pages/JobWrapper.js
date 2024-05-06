e0f617b2f9c5f46aad30a25a6292fc1520af5e9f
aayushchauhan17 <aayushchauhan176@gmail.com>
2024-05-06 09:43:00 +0530

    FEATURE: infinite scroll for a seamless browsing experience

--- src/pages/JobWrapper.js
+++ src/pages/JobWrapper.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchJobs } from '../slices/job.slice';
 import { useSelector, useDispatch } from 'react-redux';
 import JobFilters from '../components/JobFilter';
@@ -13,7 +13,24 @@ export function JobWrapper(){
         dispatch(fetchJobs());
     }, [dispatch]);
 
-    if (loading) return <div>Loading...</div>;
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.innerHeight + window.scrollY;
+            const documentHeight = document.body.scrollHeight;
+            const isScrolledNearBottom = scrollPosition + 200 >= documentHeight;
+
+            if (isScrolledNearBottom && !loading ) {
+                dispatch(fetchJobs());
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [loading, dispatch]);
+
+    // if (loading) return <div>Loading...</div>;
     if (error) return <div>Error: {error}</div>;
 
 
